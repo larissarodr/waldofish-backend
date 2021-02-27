e9025c6374e854b08e8e847d94b40abd88ee6c17
larissa_rodr@hotmail.com <larissa_rodr@hotmail.com>
2021-02-27 13:48:34 -0600

    Renaming and adding code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# wataqua-backend
+![alt text][logo]
+
+[logo]: https://github.com/larissarodr/waldofish-backend/blob/master/src/main/resources/assets/logo.png "WaldoFish"
+
+# waldofish-backend
+
+WaldoFish is a fish lab management system.
---
+++ src/main/java/com/lrodrigues/waldofish/WaldoFishApplication.java
@@ -0,0 +1,13 @@
+package com.lrodrigues.waldofish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaldoFishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaldoFishApplication.class, args);
+	}
+
+}
